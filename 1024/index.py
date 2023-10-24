140e2669a62dd581e08e2a7d6cd0a923dd37cd2a
wwwxsv19 <wwwxsv19@gmail.com>
2023-10-24 16:59:06 +0900

    ~1024

---
+++ 1024/index.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template, jsonify
+import MariaDB
+import DHT11
+
+app = Flask(__name__)
+store = MariaDB.MariaDB()
+data = DHT11.DHT11()
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route('/api/now')
+def insertdb():
+    temp, hum = data.getNow()
+    result = store.add(temp, hum)
+    return result
+
+@app.route('/api/record')
+def selectAll():
+    result = store.selectAll()
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
